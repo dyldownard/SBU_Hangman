f37238f8ce1cae8e6c570dedfa11acf6136491cd
Dylan T. Downard <dyldownard@outlook.com>
2019-11-14 19:31:05 -0500

    Set last flag for hovering

--- src/control/KeyboardViewEvents.java
+++ src/control/KeyboardViewEvents.java
@@ -2,6 +2,7 @@ package control;
 
 import javafx.scene.control.Label;
 import javafx.scene.effect.ColorAdjust;
+import view.Hangman;
 import view.KeyboardView;
 
 public class KeyboardViewEvents {
@@ -9,10 +10,10 @@ public class KeyboardViewEvents {
 	public static void setKeyboardHover(KeyboardView key) {
 		for (Label lab : key.labs) {
 			lab.setOnMouseEntered(e -> {
-				if (lab.getBackground()==null) {
-				ColorAdjust hover = new ColorAdjust();
-				hover.setBrightness(0.5);
-				lab.setEffect(hover);
+				if (lab.getBackground()==null && Hangman.gc.gameInProgress) {
+					ColorAdjust hover = new ColorAdjust();
+					hover.setBrightness(0.5);
+					lab.setEffect(hover);
 				}
 			});
 			lab.setOnMouseExited(e -> {
